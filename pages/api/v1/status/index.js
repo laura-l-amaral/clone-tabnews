67c7d95e312487f1831cc193818c8c7dd6ea0e54
Laura Amaral <100051996+laura-l-amaral@users.noreply.github.com>
2025-04-08 22:42:48 +0000

    finish status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,38 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1;");
-  console.log(result);
-  response.status(200).json({ message: "alunos são pessoas que aprendem" });
+  const updatedAt = new Date();
+
+  const postgresVersionResponse = await database.query("SHOW server_version;");
+  const postgresVersion = postgresVersionResponse.rows[0].server_version;
+
+  const postgresMaxConnectionsResponse = await database.query(
+    "SHOW max_connections;",
+  );
+  const postgresMaxConnections = parseInt(
+    postgresMaxConnectionsResponse.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  console.log(`Banco de dados selecionado: ${databaseName}`);
+  const postgresActiveConnectionsResponse = await database.query({
+    text: `select count(*) from pg_stat_activity where datname = $1;`,
+    values: [databaseName],
+  });
+  const postgresActiveConnections = parseInt(
+    postgresActiveConnectionsResponse.rows[0].count,
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgres_version: postgresVersion,
+        postgres_max_connections: postgresMaxConnections,
+        postgres_active_connections: postgresActiveConnections,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,24 @@
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const postgresVersion = responseBody.dependencies.database.postgres_version;
+  expect(postgresVersion).toEqual("16.0");
+
+  const postgresMaxConnections =
+    responseBody.dependencies.database.postgres_max_connections;
+  expect(postgresMaxConnections).toBeGreaterThan(0);
+
+  const postgresActiveConnections =
+    responseBody.dependencies.database.postgres_active_connections;
+  expect(postgresActiveConnections).toBeGreaterThan(0);
+
+  expect(responseBody.dependencies.database.postgres_active_connections).toBe(
+    1,
+  );
 });
