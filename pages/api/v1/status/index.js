59dfdcd6ef21c545341e1364bb093a53d4f59911
Laura Amaral <100051996+laura-l-amaral@users.noreply.github.com>
2025-04-08 19:03:52 +0000

    add  and

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
-import { RESPONSE_LIMIT_DEFAULT } from "next/dist/server/api-utils";
+import database from "../../../../infra/database.js";
 
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1;");
+  console.log(result);
   response.status(200).json({ message: "alunos são pessoas que aprendem" });
 }
 
